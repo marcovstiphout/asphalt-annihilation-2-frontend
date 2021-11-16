4ed3f2c6ca4522f272e02aefb3d2414b328f7dab
Marco <marcovstiphout@gmail.com>
2021-11-16 15:43:08 +0100

    Upgraded Draw Road with new assets and better generation code

--- src/app/gamescreen/models/Road.ts
+++ src/app/gamescreen/models/Road.ts
@@ -1,45 +1,63 @@
 export class Road {
-    private color = 'red';
+    private color = 'bright';
 
     private blockHeight:number;
 
-    private white_road_image = new Image();
-    private red_road_image = new Image();
-    private isRed = true;
-  
+    private dull_road_image = new Image();
+    private dull_road_striped_image = new Image();
+    private bright_road_image = new Image();
+    private bright_road_striped_image = new Image();
+
+    private isBright = true;
+    private isStriped = false;
+
     constructor(private ctx: CanvasRenderingContext2D) 
     { 
-        this.white_road_image.src = '../../assets/road_w.png';
-        this.red_road_image.src = '../../assets/road_r.png';
-        this.blockHeight = 30;
+        this.dull_road_image.src = '../../assets/road_dull.jpg';
+        this.dull_road_striped_image.src = '../../assets/road_dull_striped.jpg';
+
+        this.bright_road_image.src = '../../assets/road_bright.jpg';
+        this.bright_road_striped_image.src = '../../assets/road_bright_striped.jpg';
+
+        this.blockHeight = 150;
     }
-  
-    public draw() {
-      this.ctx.fillStyle = this.color;
+    public draw()
+    {
+        //Determine Starting Dimension
+        var initialWidth = (this.ctx.canvas.width / 4) * 3; //Makes the road width 75% of the Screen's Width. <== Does not get updated
+        var currentWidth = initialWidth; //Tracks the current width of a road piece. <== This one gets updated
 
-      var startWidth = (this.ctx.canvas.width/6)*3;//Initial road width
-      var currentWidth = startWidth;//Current width of the road pieces
+        var currentBlockHeight = this.blockHeight; //Height of the current block. <== Decreases after each block, and resets next loop.
 
+        
       var x = (this.ctx.canvas.width/2) - (currentWidth/2);//Start position on the x-axis
       var y = this.ctx.canvas.height - 1; //Start position on the y-axis
-
-      for (var i = 0; i < (innerHeight/2); i++)
-      {
-         if(this.isRed)
-         {
-            this.ctx.drawImage(this.red_road_image, x + i/2,y - i, currentWidth - i,1);
-            if(i % this.blockHeight === 0)
+        //[Loop Start]
+        for(var i = 0; i < (innerHeight/2); i++) //Ensures the road draws up to about the center of the screen.
+        {
+        //Determine whether Bright or Dull
+            if(i % currentBlockHeight === 0) 
             {
-                this.isRed = false;
-            }    
-         }
-        else{
-            this.ctx.drawImage(this.white_road_image, x + i/2,y - i, currentWidth - i,1);
-            if(i % this.blockHeight === 0)
+                currentBlockHeight -= 10;
+                this.isBright = !this.isBright;
+            }
+        //Determine whether Striped or Not
+            if(i % (currentBlockHeight/2) === 0 && i % currentBlockHeight !== 0) this.isStriped = !this.isStriped;
+        //Draw
+            var chosenImage:HTMLImageElement;
+            switch(this.isBright)
             {
-                this.isRed = true;
-            }  
+                case true:
+                    if(this.isStriped) chosenImage = this.bright_road_striped_image
+                    else chosenImage = this.bright_road_image
+                    break;
+                case false:
+                    if(this.isStriped) chosenImage = this.dull_road_striped_image
+                    else chosenImage = this.dull_road_image
+                    break;
+            }
+            this.ctx.drawImage(chosenImage, x + i/2,y - i, currentWidth - i,1);
         }
-      }
+        //[Loop End]
     }
   }
